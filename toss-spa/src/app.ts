88263f7a37d656b5e5ade6176c4483b5f63bd700
mindong2 <qlsnaos12@naver.com>
2024-02-20 19:42:48 +0900

    hire 섹션 추가 작업

--- toss-spa/src/app.ts
+++ toss-spa/src/app.ts
@@ -1,9 +1,11 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
+import HireSection from "./components/hire-section";
 import Tech from "./pages/tech";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   ${Header}
   ${Tech}
+  ${HireSection}
   ${Footer}
 `;
---
+++ toss-spa/src/components/hire-section.ts
@@ -0,0 +1,14 @@
+const HireSection = `
+    <section class="hire-section">
+        <div class="hire-wrap">
+            <img src="/img/website_code_blue_alpha.png" alt="" />
+            <div class="hire-content">
+                <span class="content-title">토스팀이 만드는 수많은 혁신의 순간들</span>
+                <p>당신과 함께 만들고 싶습니다. <br/> 지금, 토스팀에 합류하세요.</p>
+                <button class="toss-btn">채용 중인 공고 보기</button>
+            </div>
+        </div>
+    </section>
+`;
+
+export default HireSection;
