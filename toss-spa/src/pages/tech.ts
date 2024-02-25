d1a24005927a349a65e8c03c2aa526abe96b8308
mindong2 <qlsnaos12@naver.com>
2024-02-25 20:02:44 +0900

    🎨 Refactor: 공통 리스트 컴포넌트화
    Refactor: 공통 리스트 컴포넌트화

--- toss-spa/src/pages/tech.ts
+++ toss-spa/src/pages/tech.ts
@@ -1,3 +1,7 @@
+import ContentList from "../components/content-list";
+
+const List = new ContentList("tech");
+
 class Tech {
   template() {
     return `
@@ -6,33 +10,7 @@ class Tech {
                 <h2 class="content-title">개발</h2>
         
                 <section class="tech">
-                    <ul class="item-list">
-                    <li>
-                        <a href="javascript:;">
-                        <div class="list-img">
-                            <img src="https://static.toss.im/assets/payments/contents/payments-frontend-thumb-2.jpg" alt="" />
-                        </div>
-                        <div class="list-content">
-                            <h3>프론트엔드 로깅 신경 안 쓰기</h3>
-                            <p>프론트엔드 개발자라면 한 번쯤 고민해봤을 클라이언트 로깅 개선 과정을 공유합니다.</p>
-                            <span>2023.12.20</span>
-                        </div>
-                        </a>
-                    </li>
-        
-                    <li>
-                        <a href="javascript:;">
-                        <div class="list-img">
-                            <img src="https://static.toss.im/assets/payments/contents/contents-fx-thumb.jpg" alt="" />
-                        </div>
-                        <div class="list-content">
-                            <h3>인자가 많은 메서드는 왜 나쁠까?</h3>
-                            <p>인자가 많은 메서드를 함께 리팩토링 하면서 코드 사용자 입장에서 코드 복잡성을 관리하는 방법을 알아봅니다.</p>
-                            <span>2023.11.29</span>
-                        </div>
-                        </a>
-                    </li>
-                    </ul>
+                    ${List.template()}
                 </section>
             </div>
         </main>
