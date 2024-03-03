90576012b8d2e5737c0a5a123476d5313584500b
mindong2 <qlsnaos12@naver.com>
2024-03-04 00:09:34 +0900

    💄 Design: .toss-btn 스타일 수정
    Design: .toss-btn 스타일 수정

--- toss-spa/src/components/common/HireSection.ts
+++ toss-spa/src/components/common/HireSection.ts
@@ -8,7 +8,7 @@ export default class HireSection extends Component {
             <div class="hire-content">
                 <span class="content-title">토스팀이 만드는 수많은 혁신의 순간들</span>
                 <p>당신과 함께 만들고 싶습니다. <br/> 지금, 토스팀에 합류하세요.</p>
-                <button class="toss-btn">채용 중인 공고 보기</button>
+                <button class="toss-btn"><a href="https://toss.im/career/jobs">채용 바로가기</a></button>
             </div>
         </div>
 `;
