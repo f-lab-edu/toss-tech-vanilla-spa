98174c89f20fd3b09e28d764c10283cc688bd5bb
mindong2 <qlsnaos12@naver.com>
2024-03-07 20:00:14 +0900

    🐛 Fix: 일반 a 태그의 링크 이동이 안되던 현상 수정
    일반 a 태그 링크이동이 안되어 spa/route를 사용하는 a태그에 대해 dataset을 추가하여 그것을 토대로 이동시키도록 했습니다.

--- toss-spa/src/components/common/Header.ts
+++ toss-spa/src/components/common/Header.ts
@@ -6,11 +6,11 @@ export default class Header extends Component {
         <div class="header-wrap">
             <div class="header-wrap__inner">
                 <h1 id="logo">
-                    <a href="/"><img src="/img/common/logo.png" alt="tosstech"/></a>
+                    <a data-navigate="/" href="javascript:void(0);"><img src="/img/common/logo.png" alt="tosstech"/></a>
                 </h1>
                 <ul>
-                    <li><a href="/design">디자인</a></li>
-                    <li><a href="/tech">개발</a></li>
+                    <li><a href="javascript:void(0);" data-navigate="/design">디자인</a></li>
+                    <li><a href="javascript:void(0);" data-navigate="/tech">기술</a></li>
                     <li><button class="toss-btn"><a href="https://toss.im/career/jobs">채용 바로가기</a></button></li>
                 </ul>
             </div>
